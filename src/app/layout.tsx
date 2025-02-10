66ce58e6a3dcfeed1c6c4461331990386e8fb709
zer0 <ryanfanno87@gmail.com>
2025-02-11 02:04:49 +0800

    fourth commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export const metadata: Metadata = {
     locale: 'en_AU',
     type: 'website',
   },
+  other: {
+    'google-adsense-account': 'ca-pub-5075448607477357',
+  },
 };
 
 export default function RootLayout({
