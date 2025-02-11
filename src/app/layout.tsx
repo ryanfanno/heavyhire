fcadbdc3de07652c4ecb26a8c20d3c705c60cddf
zer0 <ryanfanno87@gmail.com>
2025-02-11 20:25:39 +0800

    tenth commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,31 @@ export const metadata: Metadata = {
     'google-adsense-account': 'ca-pub-5075448607477357',
   },
   icons: {
-    icon: '/favicon.ico',
-    // Optional: Add other sizes/formats
-    // apple: '/apple-icon.png',
-    // shortcut: '/favicon-16x16.png',
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: {
+      url: '/apple-touch-icon.png',
+      sizes: '180x180',
+      type: 'image/png',
+    },
+    other: [
+      {
+        rel: 'manifest',
+        url: '/site.webmanifest',
+      },
+      {
+        rel: 'android-chrome',
+        url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+      },
+      {
+        rel: 'android-chrome',
+        url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+      },
+    ],
   },
 };
 
